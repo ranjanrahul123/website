e67491687a9d6543e53be195f89c5e8b0498d249
Shakkir Moulana <41133926+shakkirmln@users.noreply.github.com>
2020-04-30 22:24:49 +0530

    Update dbh.inc.php

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -2,7 +2,7 @@
     
     $servername ="localhost:3306";
     $dbusername ="id13504856_admin";
-    $dbpassword ="0_y(LUhDf\aA?3DA";
+    $dbpassword ="ew_AGjB2kjqsAji";
     $dbname ="id13504856_cgpaa";
 
    $conn = mysqli_connect($servername, $dbusername,$dbpassword, $dbname) ;
